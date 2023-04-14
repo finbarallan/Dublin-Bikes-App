f753d9485ab079d83a6d324c522eec88642a0d6a
Finbar Allan <116646659+finbarallan@users.noreply.github.com>
2023-04-14 20:17:53 +0100

    predictive-model-gui

--- web/static/js/predictive.js
+++ web/static/js/predictive.js
@@ -1,32 +1,71 @@
 document.getElementById('prediction-form').addEventListener('submit', function (event) {
-  event.preventDefault();
+    event.preventDefault();
+  
+    // Get user input values
+    const stationNumber = document.getElementById('station-number').value;
+    const date = document.getElementById('date').value;
+    const hour = document.getElementById('hour').value;
+  
+    // Fetch weather data from OpenWeather API
+    function fetchWeatherData(date, hour) {
+        const apiKey = "e27ae2a7d7babf283e771962901cd48f";
+        const city = "Dublin";
+        const dateString = `${date} ${hour}:00:00`;
+        console.log(dateString);
+        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+      
+        return fetch(apiUrl)
+          .then(response => {
+            console.log(response);
+            return response.json();
+          });
+      }
 
-  // Get user input values
-  const stationNumber = document.getElementById('station-number').value;
-  const day = document.getElementById('day').value;
-  const hour = document.getElementById('hour').value;
-
-  const weather = document.getElementById('weather').value; // from API call
-  const description = document.getElementById('description').value; // from API call
-  const temp = document.getElementById('temp').value; // from API call
-  const feelsLike = document.getElementById('feels-like').value; // from API call
-  const humidity = document.getElementById('humidity').value; // from API call
-  const windSpeed = document.getElementById('wind-speed').value; // from API call
-
-  // Send POST request to Flask route
-  fetch('/predict', {
-    method: 'POST',
-    body: new FormData(document.getElementById('prediction-form'))
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Display predicted values in HTML
-      document.getElementById('prediction-results').innerHTML = `
-            <p>Predicted available bikes: ${data.available_bikes}</p>
-            <p>Predicted available stands: ${data.available_stands}</p>
-        `;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-});
+      console.log(date)
+      console.log(hour)
+      
+      fetchWeatherData(date, hour)
+      .then(weatherData => {
+        // Get the weather data for the specified date and hour
+        const forecastData = weatherData.list.filter(item => {
+          const itemDate = new Date(item.dt_txt);
+          return itemDate.getDate() === parseInt(date) && itemDate.getHours() === parseInt(hour);
+        })[0];
+        
+        console.log(forecastData);
+    
+        const weather = forecastData.weather[0].main;
+        const description = forecastData.weather[0].description;
+        const temp = forecastData.main.temp;
+        const feelsLike = forecastData.main.feels_like;
+        const humidity = forecastData.main.humidity;
+        const windSpeed = forecastData.wind.speed;
+  
+        // Create FormData object and append all required data
+        const formData = new FormData(document.getElementById('prediction-form'));
+        formData.append('weather', weather);
+        formData.append('description', description);
+        formData.append('temp', temp);
+        formData.append('feels_like', feelsLike);
+        formData.append('humidity', humidity);
+        formData.append('wind_speed', windSpeed);
+  
+        // Send POST request to Flask route
+        return fetch('/predict', {
+          method: 'POST',
+          body: formData
+        });
+      })
+      .then(response => response.json())
+      .then(data => {
+        // Display predicted values in HTML
+        document.getElementById('prediction-results').innerHTML = `
+              <p>Predicted available bikes: ${data.available_bikes}</p>
+              <p>Predicted available stands: ${data.available_stands}</p>
+          `;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
+  
